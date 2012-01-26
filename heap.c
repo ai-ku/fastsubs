0f408f8d3931ec3f11817215f23fd79d4a5865f4
Deniz Yuret <denizyuret@gmail.com>
2012-01-26 21:59:06 +0000

    Initial revision

---
+++ heap.c
@@ -0,0 +1,76 @@
+#include "heap.h"
+
+/* With a one based binary heap parent is at i/2 and children are at
+   2i, 2i+1. */
+
+void heap_insert_min(Heap heap, Token key, gfloat value) {
+  guint n = ++heap_size(heap);
+  while (n > 1) {
+    guint p = n >> 1;
+    if (heap[p].logp <= value) break; /*  */
+    heap[n] = heap[p];
+    n = p;
+  }
+  heap[n].token = key;
+  heap[n].logp = value;
+}
+
+void heap_insert_max(Heap heap, Token key, gfloat value) {
+  guint n = ++heap_size(heap);
+  while (n > 1) {
+    guint p = n >> 1;
+    if (heap[p].logp >= value) break; /*  */
+    heap[n] = heap[p];
+    n = p;
+  }
+  heap[n].token = key;
+  heap[n].logp = value;
+}
+
+LMpair heap_delete_min(Heap heap) {
+  LMpair top = heap[1];
+  LMpair bot = heap[heap_size(heap)];
+  guint end = --heap_size(heap);
+  guint p = 1;
+  guint n;
+  while ((n = p << 1) <= end) {
+    if ((n < end) && (heap[n+1].logp <= heap[n].logp)) n++; /*  */
+    if (bot.logp <= heap[n].logp) break;		    /*  */
+    heap[p] = heap[n];
+    p = n;
+  }
+  heap[p] = bot;
+  return top;
+}
+
+LMpair heap_delete_max(Heap heap) {
+  LMpair top = heap[1];
+  LMpair bot = heap[heap_size(heap)];
+  guint end = --heap_size(heap);
+  guint p = 1;
+  guint n;
+  while ((n = p << 1) <= end) {
+    if ((n < end) && (heap[n+1].logp >= heap[n].logp)) n++; /*  */
+    if (bot.logp >= heap[n].logp) break;		    /*  */
+    heap[p] = heap[n];
+    p = n;
+  }
+  heap[p] = bot;
+  return top;
+}
+
+void heap_sort_max(Heap heap) {
+  guint size = heap_size(heap);
+  for (guint i = size; i > 1; i--) {
+    heap[i] = heap_delete_min(heap);
+  }
+  heap_size(heap) = size;
+}
+
+void heap_sort_min(Heap heap) {
+  guint size = heap_size(heap);
+  for (guint i = size; i > 1; i--) {
+    heap[i] = heap_delete_max(heap);
+  }
+  heap_size(heap) = size;
+}
---
+++ heap.h
@@ -0,0 +1,34 @@
+#ifndef __HEAP_H__
+#define __HEAP_H__
+#include <glib.h>
+#include "token.h"
+
+/* heap is an array of token-logp pairs. */
+typedef struct _LMpair {
+  Token token;
+  gfloat logp;
+} LMpair, *Heap;
+
+/* The first element is used to store the count n, so the actual
+   elements go from 1..n.  */
+#define heap_size(h) ((h)[0].token)
+#define heap_top(h) ((h)[1])
+
+/* The min/max suffixes indicate which element ends up at heap[1] */
+/* heap_insert_min, heap_delete_min go together and can be followed by heap_sort_max. */
+/* heap_insert_max, heap_delete_max go together and can be followed by heap_sort_min. */
+
+/* This inserts a new element so heap[1] has the min/max logp value. */
+void heap_insert_min(Heap heap, Token key, gfloat value);
+void heap_insert_max(Heap heap, Token key, gfloat value);
+
+/* This deletes the top element of a min/max heap and fixes the rest of the heap. */
+LMpair heap_delete_min(Heap heap);
+LMpair heap_delete_max(Heap heap);
+
+/* this sorts the min/max heapified array so heap[1] has the max/min
+   logp value.  Note that the order of heap[1] is reversed. */
+void heap_sort_max(Heap heap);
+void heap_sort_min(Heap heap);
+
+#endif
