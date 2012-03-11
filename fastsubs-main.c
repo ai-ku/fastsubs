fb885620c00615e81fd917e9b4a06877396fe7bc
Deniz Yuret <denizyuret@gmail.com>
2012-03-11 07:13:51 +0000

    Change defaults for nlimit and plimit to max, to use or instead of and.

--- fastsubs-main.c
+++ fastsubs-main.c
@@ -5,16 +5,22 @@
 #include "fastsubs.h"
 #include "heap.h"
 #include "minialloc.h"
-const char *usage = "Usage: fastsubs [-n <n> | -p <p>] model.lm < input.txt";
+
+#define BUF  (1<<16)		/* max input line length */
+#define SMAX (1<<16)		/* max tokens in a sentence */
+#define PMAX 1.0		/* default value for -p */
+#define NMAX G_MAXUINT		/* default value for -n */
+
 
 int main(int argc, char **argv) {
+  const char *usage = "Usage: fastsubs [-n <n> | -p <p>] model.lm < input.txt";
   g_message_init();
-  char buf[1024];
-  Token s[1025];
+  char buf[BUF];
+  Token s[SMAX+1];
 
   int opt;
-  int opt_n = 0;
-  gdouble opt_p = 0.0;
+  guint opt_n = NMAX;
+  gdouble opt_p = PMAX;
   while ((opt = getopt(argc, argv, "p:n:")) != -1) {
     switch(opt) {
     case 'n':
@@ -29,15 +35,13 @@ int main(int argc, char **argv) {
   }
   if (optind >= argc)
       g_error(usage);
-  if ((opt_n == 0) && (opt_p == 0))
-      g_error(usage);
-  g_message("Minimum substitutes -n=%d, minimum probability -p=%g", opt_n, opt_p);
+  g_message("Minimum substitutes -n=%d, or minimum probability -p=%g", opt_n, opt_p);
   g_message("Loading model file %s", argv[optind]);
   LM lm = lm_init(argv[optind]);
   Hpair *subs = minialloc(lm->nvocab * sizeof(Hpair));
   g_message("ngram order = %d\n==> Enter sentences:\n", lm->order);
-  while(fgets(buf, 1024, stdin)) {
-    int n = sentence_from_string(s, buf, 1024);
+  while(fgets(buf, BUF, stdin)) {
+    int n = sentence_from_string(s, buf, SMAX);
     for (int i = 2; i <= n; i++) {
       int nsubs = fastsubs(subs, s, i, lm, opt_p, opt_n);
       printf("%s", token_to_string(s[i]));
