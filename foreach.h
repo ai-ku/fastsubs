60c9a4e4d631605864ab75bd7790f60f48522db0
Deniz Yuret <denizyuret@gmail.com>
2012-05-26 17:58:23 +0300

    New foreach.h, procinfo.h, .gitignore

--- foreach.h
+++ foreach.h
@@ -1,43 +1,40 @@
-/* $Id$ */
+/* foreach.h: C needs some good looping macros.
+ *
+ * Copyright (c) 2004-2012, Deniz Yuret <denizyuret@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef __FOREACH_H__
 #define __FOREACH_H__
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 
 #include <stdio.h>
 #include <string.h>
-#include <glib.h>
 #include <errno.h>
 #include <stdlib.h>
+#define LINE (1<<20)
 
-/* FOREACH: C needs some good looping macros: */
+/* foreach_line(str, file): iterates with str = each line of file */
+/* Use file = path string for a regular file */
+/* Use file = empty string to represent stdin */
+/* Use file = "< cmd" to represent the output of a command */
 
-#define foreach(type, var, array)\
-  for (register type (var), *_p = (type*)(array)->pdata;\
-       _p != NULL; _p = NULL)\
-  for (register int _i = 0, _l = (array)->len;\
-       (_i < _l) && ((var = _p[_i]) || 1); _i++)
-
-#define foreach_ptr(ptr, array)\
-  for (register gpointer *ptr = (array)->pdata;\
-       ptr != NULL; ptr = NULL)\
-  for (register int _i = 0, _l = (array)->len;\
-       (_i < _l); _i++, ptr++)
-
-#define foreach_int(var, lo, hi)\
-  for (register int var = (lo), _hi = (hi); var <= _hi; var++)
-
-#define foreach_char(var, str)\
-  for (register char var, *_p = (str);\
-       (var = *_p) != 0; _p++)
-
-#define LINE (1<<16)
-
-/* Use the empty string to represent stdin */
-/* Use "|cmd" to represent the output of a command */
-/* Use path string for a regular file */
-
-#define _myopen(f) ((*(f)==0)?stdin:(*(f)=='|')?popen((f)+1,"r"):fopen((f),"r"))
-#define _myclose(f,fp) ((*(f)==0)?0:(*(f)=='|')?pclose(fp):fclose(fp))
+#define _myopen(f) ((*(f)==0)?stdin:(*(f)=='<')?popen((f)+1,"r"):fopen((f),"r"))
+#define _myclose(f,fp) ((*(f)==0)?0:(*(f)=='<')?pclose(fp):fclose(fp))
 
 #define foreach_line(str, fname)\
   errno = 0; \
@@ -57,6 +54,27 @@
 #define foreach_token3(tok, str, sep)\
   for (char *_ptr = NULL, *(tok) = strtok_r((str), (sep), &_ptr);\
        (tok) != NULL; (tok) = strtok_r(NULL, (sep), &_ptr))
+
+#define foreach_int(var, lo, hi)\
+  for (register int var = (lo), _hi = (hi); var <= _hi; var++)
+
+#define foreach_char(var, str)\
+  for (register char var, *_p = (str);\
+       (var = *_p) != 0; _p++)
+
+#ifdef __G_LIB_H__
+#define foreach(type, var, array)\
+  for (register type (var), *_p = (type*)(array)->pdata;\
+       _p != NULL; _p = NULL)\
+  for (register int _i = 0, _l = (array)->len;\
+       (_i < _l) && ((var = _p[_i]) || 1); _i++)
+
+#define foreach_ptr(ptr, array)\
+  for (register gpointer *ptr = (array)->pdata;\
+       ptr != NULL; ptr = NULL)\
+  for (register int _i = 0, _l = (array)->len;\
+       (_i < _l); _i++, ptr++)
+#endif
 
 #endif
 
--- procinfo.h
+++ procinfo.h
@@ -1,39 +1,58 @@
-/* $Id$ */
+/* procinfo.h: reporting the time and memory usage of a process.
+ *
+ * Copyright (c) 2004-2012, Deniz Yuret <denizyuret@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef __PROCINFO_H__
 #define __PROCINFO_H__
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <glib.h>
-#include "foreach.h"
+#include <string.h>
 
 #define dot(n) (((n)==0) ? (_procinfo_cnt = (fputc('\n', stderr), 0)) :\
 		(++_procinfo_cnt && ((n)<64)) ? fputc('.', stderr) :\
 		(_procinfo_cnt % ((n)>>2) == 0) ? fprintf(stderr, "%d%%", 25*_procinfo_cnt/(n>>2)) :\
 		(_procinfo_cnt % ((n)>>5) == 0) ? fputc('.', stderr) : 0)
 
-guint32 _procinfo_cnt = 0;
+unsigned long _procinfo_cnt = 0;
 
-static guint64 memory() {
-  guint64 mem = 0;
-#ifndef CYGWIN
-  int i = 0;
-  foreach_line(buf, "/proc/self/stat") {
-    foreach_token(tok, buf) {
-      if (++i == 23) {
+static unsigned long long memory() {
+  static char buf[1024];
+  FILE *fp;
+  unsigned long long mem = 0;
+  if ((fp = fopen("/proc/self/stat", "r")) != NULL) {
+    if (fgets(buf, 1024, fp)) {
+      char *tok = strtok(buf, " ");
+      for (int i = 1; i < 23; i++) {
+	tok = strtok(NULL, " ");
+      }
+      if (tok != NULL) {
 	mem = atoll(tok);
-	break;
       }
     }
-    break;
   }
-#endif
   return mem;
 }
 
-static guint32 runtime() {
+static unsigned long runtime() {
   static time_t t0 = 0;
   time_t t1 = time(NULL);
   if (t0 == 0) t0 = t1;
@@ -53,11 +72,13 @@ static guint32 runtime() {
    which has a resolution of seconds.
 */
 
+#ifdef __G_LIB_H__
+
 static void my_log_func(const gchar *log_domain,
 		 GLogLevelFlags log_level,
 		 const gchar *message,
 		 gpointer user_data) {
-  fprintf(stderr, "[t=%d m=%" G_GUINT64_FORMAT "] %s\n", 
+  fprintf(stderr, "[t=%lu m=%llu] %s\n", 
 	  runtime(), memory(), message);
 }
 
@@ -65,5 +86,7 @@ static void g_message_init()
 {
   g_log_set_handler(NULL, G_LOG_LEVEL_MESSAGE, my_log_func, NULL);
 }
+
+#endif
 
 #endif
