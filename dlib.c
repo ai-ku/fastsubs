c2750f94d4674e6947a1e5c02376d2a0050e04b1
Deniz Yuret <denizyuret@gmail.com>
2013-12-29 23:14:10 +0200

    Added dlib files

---
+++ dlib.c
@@ -0,0 +1,318 @@
+/* Support code for dlib, see dlib.h for documentation. */
+
+#include "dlib.h"
+#include <stdio.h>		/* NULL, FILE, stdin, fopen, fclose,
+				   (popen, pclose, getline if -D_GNU_SOURCE) */
+#include <stdlib.h>		/* NULL, EXIT_FAILURE, free */
+#include <string.h>		/* strlen */
+#include <errno.h>		/* errno */
+#include <time.h>		/* clock_t, clock */
+#include <stdarg.h>		/* va_start etc. */
+#if D_HAVE_MUSABLE
+#include <malloc.h>		/* malloc_usable_size */
+#endif
+#if D_HAVE_ZLIB
+#include <zlib.h>		/* gzopen, gzclose, gzgets */
+#endif
+
+/*** msg and die support code */
+
+static void _d_error_clock(double c) {
+  if (c > 3600) fprintf(stderr, "%ldh", (long) (c / 3600));
+  if (c > 60) fprintf(stderr, "%ldm", ((long) (c / 60)) % 60);
+  fprintf(stderr, "%.2fs", c - 60 * ((long) (c / 60)));
+}
+
+#if D_HAVE_PROC
+static void _d_error_mem(size_t m) {
+  if (m < 1000) {
+    fprintf(stderr, "%zu", m);
+  } else { 
+    _d_error_mem(m / 1000);
+    fprintf(stderr, ",%03zu", m % 1000);
+  }    
+}
+#endif
+
+void _d_error(int status, int errnum, const char *format, ...) {
+  fflush(stdout);
+  putc('[', stderr);
+  double c = (double) clock() / CLOCKS_PER_SEC;
+  _d_error_clock(c);
+#if D_HAVE_MUSABLE
+  putc(' ', stderr);
+  _d_error_mem(_d_memsize);
+#endif
+#if D_HAVE_PROC
+  putc(' ', stderr);
+  char *tok[23];
+  forline(l, "/proc/self/stat") {
+    split(l, ' ', tok, 23); break;
+  }
+  _d_error_mem(strtoul(tok[22], NULL, 10));
+  putc('b', stderr);
+#endif
+  fputs("] ", stderr);
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  if (errnum) {
+    fprintf(stderr, ": %s", strerror(errnum));
+    errno = 0;
+  }
+  putc('\n', stderr);
+  if (status) exit(status);
+}
+
+/*** error checking memory allocation */
+
+size_t _d_memsize = 0;
+
+void *_d_malloc(size_t size) {
+  void *ptr = malloc(size);
+  if (ptr == NULL) 
+    die("Cannot allocate %zu bytes", size);
+#if D_HAVE_MUSABLE
+  _d_memsize += malloc_usable_size(ptr);
+#endif
+  return ptr;
+}
+
+void *_d_calloc(size_t nmemb, size_t size) {
+  void *ptr = calloc(nmemb, size);
+  if (ptr == NULL) 
+    die("Cannot allocate %zu bytes", nmemb*size);
+#if D_HAVE_MUSABLE
+  _d_memsize += malloc_usable_size(ptr);
+#endif
+  return ptr;
+}
+
+void *_d_realloc(void *ptr, size_t size) {
+#if D_HAVE_MUSABLE
+  _d_memsize -= malloc_usable_size(ptr);
+#endif
+  void *ptr2 = realloc(ptr, size);
+  if (ptr2 == NULL) 
+    die("Cannot allocate %zu bytes", size);
+#if D_HAVE_MUSABLE
+  _d_memsize += malloc_usable_size(ptr2);
+#endif
+  return ptr2;
+}
+
+void _d_free(void *ptr) {
+#if D_HAVE_MUSABLE
+  assert(_d_memsize >= malloc_usable_size(ptr));
+  _d_memsize -= malloc_usable_size(ptr);
+#endif
+  free(ptr);
+}
+
+/*** forline support code */
+
+struct _D_FILE_S {
+  void *fptr;
+  enum { _D_STDIN, _D_POPEN, _D_FOPEN, _D_GZOPEN } type;
+  size_t size;
+  char *line;
+};
+
+#if D_HAVE_ZLIB
+static int _d_gzfile(const char *f) {
+  FILE *fp = fopen(f, "r");
+  if (fp == NULL) return 0;
+  int c1 = fgetc(fp);
+  int c2 = fgetc(fp);
+  fclose(fp);
+  return ((c1 == 0x1f) && (c2 == 0x8b));
+}
+#endif
+
+_D_FILE _d_open(const char *f) {
+  _D_FILE p = _d_malloc(sizeof(struct _D_FILE_S));
+  p->size = 0;
+  p->line = NULL;
+  if (f == NULL) {
+    p->fptr = stdin;
+    p->type = _D_STDIN;
+#if D_HAVE_POPEN
+  } else if (*f == '<') {
+    p->fptr = popen(f+1, "r");
+    p->type = _D_POPEN;
+#endif
+#if D_HAVE_ZLIB
+  } else if (_d_gzfile(f)) {
+    p->fptr = gzopen(f, "r");
+    p->type = _D_GZOPEN;
+#endif
+  } else {
+    p->fptr = fopen(f, "r");
+    p->type = _D_FOPEN;
+  }
+  if (p->fptr == NULL) {
+    die("Cannot open %s", f);
+  }
+  return p;
+}
+
+void _d_close(_D_FILE p) {
+  free(p->line);		// may be reallocated by getline
+  switch(p->type) {
+  case _D_FOPEN: fclose(p->fptr); break;
+#if D_HAVE_POPEN
+  case _D_POPEN: pclose(p->fptr); break;
+#endif
+#if D_HAVE_ZLIB   
+  case _D_GZOPEN: gzclose(p->fptr); break;
+#endif
+  default: break;
+  }
+  _d_free(p);
+}
+
+char *_d_gets(_D_FILE p) {
+  if (p == NULL) return NULL;
+#if D_HAVE_GETLINE
+  if (p->type != _D_GZOPEN) {
+    ssize_t ret = getline(&(p->line), &(p->size), p->fptr);
+    return ((ret == -1) ? NULL : p->line);
+  }
+#endif
+  if (p->line == NULL || p->size == 0) {
+    p->size = 120;
+    p->line = malloc(p->size);	// can be realloced by getline
+    assert(p->line != NULL);
+  }
+  char *ptr = p->line;
+  size_t len = p->size;
+  char *ret = NULL;
+  do {
+    ptr[0] = 0;
+    ptr[len-1] = 1; // This will become 0 if line too long
+#if D_HAVE_ZLIB
+    if (p->type == _D_GZOPEN)
+      ret = gzgets(p->fptr, ptr, len);
+    else
+#endif
+      ret = fgets(ptr, len, p->fptr);
+    if (ret == NULL) break;
+    if (ptr[len-1] == 0) {  // We may need to keep reading
+      if (ptr[len-2] == '\n') break; // Just finished a line
+      size_t oldn = p->size;
+      p->size <<= 1;
+      p->line = _d_realloc(p->line, p->size);
+      ptr = &(p->line)[oldn-1];
+      len = oldn + 1;
+    }
+  } while (ptr[len-1] == 0);
+  if ((ret == NULL) && (ptr == p->line)) {
+    return NULL;
+  } else {
+    return p->line;
+  }
+}
+
+size_t fnv1a(const char *k) {
+  size_t hash = 14695981039346656037ULL;
+  uint8_t *p = (uint8_t *) k;
+  while (*p != 0) {
+    hash ^= *p++;
+    hash *= 1099511628211ULL;
+  }
+  return hash;
+}
+
+/*** fast memory allocation */
+
+#define _D_MSIZE (1<<20)
+static char *_d_mlast = NULL;
+char *_d_mfree = NULL;
+size_t _d_mleft = 0;
+#define _d_mnext(m) (*((ptr_t*)(m)))
+
+/* only way to free is to free everything */
+
+void dfreeall() {
+  while (_d_mlast != NULL) {
+    ptr_t p = _d_mnext(_d_mlast);
+    _d_free(_d_mlast);
+    _d_mlast = p;
+  }
+}
+
+ptr_t _dalloc_helper(size_t size) {
+  char *ptr = NULL;
+  if (size <= (_D_MSIZE >> 1)) {
+    // Suspicious optimization
+    // hoping realloc will not move memory if getting smaller.
+    if (_d_mlast != NULL) {
+      char *shrink = _d_realloc(_d_mlast, _d_mfree - _d_mlast);
+      if (shrink != _d_mlast) die("dalloc: suspicious optimization broke the code.");
+    }
+    ptr_t old = _d_mlast;
+    _d_mlast = _d_malloc(_D_MSIZE + sizeof(ptr_t));
+    _d_mnext(_d_mlast) = old;
+    ptr = _d_mlast + sizeof(ptr_t);
+    _d_mfree = ptr + size;
+    _d_mleft = _D_MSIZE - size;
+  } else {
+    ptr = _d_malloc(size + sizeof(ptr_t));
+    if (_d_mlast == NULL) {
+      _d_mnext(ptr) = NULL;
+      _d_mlast = ptr;
+    } else {
+      _d_mnext(ptr) = _d_mnext(_d_mlast);
+      _d_mnext(_d_mlast) = ptr;
+    }
+    ptr += sizeof(ptr_t);
+  }
+  return ptr;
+}
+
+/*** symbol table */
+
+static darr_t _d_strtable;
+static darr_t _d_symtable;
+
+#define _d_sym2str(u) (((str_t *)(_d_strtable->data))[u-1])
+#define _d_iszero(u) ((u)==0)
+#define _d_mkzero(u) ((u)=0)
+
+static sym_t _d_syminit(const str_t s) {
+  if (_d_strtable == NULL) _d_strtable = darr_new(0, sizeof(str_t));
+  size_t l = len(_d_strtable);
+  val(str_t, _d_strtable, l) = dstrdup(s);
+  return l+1;
+}
+
+D_HASH(_d_sym, sym_t, str_t, d_strmatch, fnv1a, _d_sym2str, _d_syminit, _d_iszero, _d_mkzero)
+
+sym_t str2sym(const str_t str, bool insert) {
+  if (_d_symtable == NULL) _d_symtable = _d_symnew(0);
+  sym_t *p = _d_symget(_d_symtable, str, insert);
+  return ((p == NULL) ? 0 : (*p));
+}
+
+str_t sym2str(sym_t sym) {
+  if ((sym == 0) || (_d_strtable == NULL) || (len(_d_strtable) < sym)) {
+    return NULL;
+  } else {
+    return (((str_t *)(_d_strtable->data))[sym - 1]);
+  }
+}
+
+void symtable_free() {
+  _d_symfree(_d_symtable); _d_symtable = NULL;
+  darr_free(_d_strtable); _d_strtable = NULL;
+}
+
+void symdbg() {
+  msg("str_t=%lu", sizeof(str_t));
+  msg("sym_t=%lu", sizeof(sym_t));
+  msg("strcap=%lu", _d_strtable == NULL ? 0 : cap(_d_strtable));
+  msg("strlen=%lu", _d_strtable == NULL ? 0 : len(_d_strtable));
+  msg("symcap=%lu", _d_symtable == NULL ? 0 : cap(_d_symtable));
+  msg("symlen=%lu", _d_symtable == NULL ? 0 : len(_d_symtable));
+}
---
+++ dlib.h
@@ -0,0 +1,330 @@
+/* dlib.h: Deniz's C Library
+   Copyright (c) 2013-2014, Deniz Yuret <denizyuret@gmail.com> */
+
+#ifndef __DLIB_H__
+#define __DLIB_H__
+
+/* We try to stick with C99 but use some GNU extensions if available.
+   I will try to put flags for non-C99 stuff here.  If you want the
+   following to be used, set them to 1, use gcc and compile with
+   -D_GNU_SOURCE. */
+
+#define D_HAVE_POPEN 1
+#define D_HAVE_GETLINE 1
+#define D_HAVE_PROC 1
+#define D_HAVE_MUSABLE 1
+
+/* If you have zlib and want support for reading gzip compressed files
+   set the following to 1 and compile with -lz. */
+
+#define D_HAVE_ZLIB 1
+
+/* Standard C99 includes */
+
+#include <stdlib.h>		/* NULL, EXIT_SUCCESS, EXIT_FAILURE */
+#include <string.h>		/* strspn, strpbrk */
+#include <stdint.h>		/* uint8_t etc. */
+#include <stdbool.h>		/* bool, true, false */
+#include <errno.h>		/* errno */
+#include <assert.h>		/* assert */
+
+/* Define some convenience types */
+typedef char *str_t;
+typedef void *ptr_t;
+
+/* msg and die write runtime and memory info, a formatted string, and
+   any system error message to stderr.  The only difference is die
+   exits the program, msg does not.  Example usage: 
+
+   die("Oh no I am dying: %d", 42);
+
+*/
+
+extern void _d_error(int status, int errnum, const char *format, ...);
+#define msg(...) _d_error(EXIT_SUCCESS, errno, __VA_ARGS__)
+#define die(...) _d_error(EXIT_FAILURE, errno, __VA_ARGS__)
+
+
+/* forline(l, f) { ... } is an iteration construct which executes the
+   statements in the body with the undeclared variable l set to each
+   line in file f.  If f==NULL stdin is read, if f starts with '<' as
+   in f=="< cmd args" the cmd is run with args and its stdout is read,
+   otherwise a regular file with path f is read.  If zlib is
+   available, gzip compressed files are automatically handled.
+   Example:
+
+   forline (str, "file.txt") {
+     printf("%d\n", strlen(str));  // prints the length of each line in "file.txt"
+   }
+
+*/
+
+
+#define forline(l, f)							\
+  for (_D_FILE _f_ = _d_open(f); _f_ != NULL; _d_close(_f_), _f_ = NULL)	\
+    for (char *l = _d_gets(_f_); l != NULL; l = _d_gets(_f_))
+
+typedef struct _D_FILE_S *_D_FILE;
+extern _D_FILE _d_open(const char *fname);
+extern void _d_close(_D_FILE f);
+extern char *_d_gets(_D_FILE f);
+
+
+/* fortok(t, s) { ... } is an iteration construct which executes the
+   statements in the body with the undeclared variable t set to each
+   whitespace separated token in string s.  It modifies and tokenizes
+   s the same way strtok does, but unlike strtok it is reentry safe
+   (i.e. multiple nested fortok loops are ok).  fortok3 takes an
+   additional argument d to specify delimiter characters.  Example:
+
+   char *str = strdup("  To be    or not");
+   fortok (tok, str) {
+     printf("%s:", tok); // prints To:be:or:not:
+   }
+   free(str);
+
+*/
+
+#define fortok(t, s) fortok3(t, s, " \f\n\r\t\v")
+
+#define fortok3(t, s, d)						\
+  for (char *t = (s)+strspn((s),(d)), *_p_ = strpbrk(t, (d));		\
+       ((*t != '\0') && ((_p_ == NULL) || (*_p_ = '\0', _p_++)));	\
+       ((_p_ == NULL) ? t += strlen(t) :				\
+	(t = (_p_)+strspn(_p_,(d)), _p_ = strpbrk(t, (d)))))
+
+
+/* split the str into tokens delimited by the character sep and set
+   the pointers in argv to successive tokens.  argv should have enough
+   space to hold argv_len pointers.  Stop when argv_len tokens reached
+   or str runs out.  Modifies str by replacing occurrences of sep with
+   '\0'.  Returns the number of tokens placed in argv. */
+
+static inline size_t split(char *str, int sep, char **argv, size_t argv_len) {
+  if (argv_len == 0) return 0;
+  argv[0] = str;
+  if (sep == 0) return 1;  // only one token if sep == 0
+  size_t numtokens = 1;
+  for (char *p = strchr(str, sep); p != NULL; p = strchr(p, sep)) {
+    *p++ = '\0';
+    if (numtokens == argv_len) break;
+    argv[numtokens++] = p;
+  }
+  return numtokens;
+}
+
+/* error checking memory allocation */
+extern size_t _d_memsize;
+extern void *_d_malloc(size_t size);
+extern void *_d_calloc(size_t nmemb, size_t size);
+extern void *_d_realloc(void *ptr, size_t size);
+extern void _d_free(void *ptr);
+
+
+/* fast memory allocation */
+extern char *_d_mfree;
+extern size_t _d_mleft;
+extern ptr_t _dalloc_helper(size_t size);
+extern void dfreeall();
+
+static inline ptr_t dalloc(size_t size) {
+  if (size > _d_mleft) return _dalloc_helper(size);
+  char *ptr = _d_mfree;
+  _d_mfree += size;
+  _d_mleft -= size;
+  return ptr;
+}
+
+static inline str_t dstrdup (const str_t s) {
+  size_t len = strlen (s) + 1;
+  str_t new = dalloc (len);
+  return (str_t) memcpy (new, s, len);
+}
+
+/* This has terrible performance, better comment to not encourage
+   people to use it. */
+/*
+static inline ptr_t drealloc(ptr_t ptr, size_t oldsize, size_t newsize) {
+  if (ptr == NULL) {
+    return dalloc(newsize);
+  } 
+  if (ptr == _d_mfree - oldsize) {     // last allocated
+    int64_t extra = newsize - oldsize; // could be negative
+    if (_d_mleft >= extra) {
+      _d_mfree += extra;
+      _d_mleft -= extra;
+      return ptr;
+    }
+  }
+  if (newsize <= oldsize) return ptr;
+  ptr_t ptr2 = dalloc(newsize);
+  memcpy(ptr2, ptr, oldsize);
+  return ptr2;
+}
+*/
+
+
+/* define generic container */
+
+typedef struct darr_s {
+  void *data;
+  uint64_t bits;
+} *darr_t;
+
+/* Represent log2(capacity) in the first 6 bits and number of elements
+   (len) in the last 58 bits of a uint64_t in darr_t->bits.  This
+   means capacity is always a power of 2. */
+
+#define _D_LENBITS 58
+#define cap(a) (1ULL << ((a)->bits >> _D_LENBITS))
+#define len(a) ((a)->bits & ((1ULL << _D_LENBITS) - 1))
+#define _d_dblcap(a) ((a)->bits += (1ULL << _D_LENBITS))
+#define _d_inclen(a) ((a)->bits++)
+#define _d_setlen(a,l) ((a)->bits = ((((a)->bits >> _D_LENBITS) << _D_LENBITS) | (l)))
+
+/* Define initializer and destructor */
+
+static inline darr_t darr_new(size_t nmemb, size_t esize) {
+  if (nmemb >= (1ULL << _D_LENBITS))
+    die("darr_t cannot hold more than %lu elements.", (1ULL<<_D_LENBITS));
+  darr_t a = _d_malloc(sizeof(struct darr_s));
+  size_t b; for (b = 0; (1ULL << b) < nmemb; b++);		
+  a->bits = (b << _D_LENBITS);					
+  size_t c = (1ULL << b);					
+  a->data = _d_malloc(c * esize);
+  return a;							
+}
+
+static inline void darr_free(darr_t a) {
+  _d_free(a->data); _d_free(a);
+}
+
+/* for access we like lvalue type access, instead of set, get:
+   e.g. x = val(int,a,i) or val(int,a,i) = x 
+   in this case len(a) has to give last accessed element, not necessarily last set.
+   this risks the user to accidentally allocate a huge array, oh well.
+*/
+
+#define val(t,a,i) (((t*)(_d_boundcheck((a),(i),sizeof(t))->data))[i])
+
+static inline darr_t _d_boundcheck(darr_t a, size_t i, size_t esize) {
+  size_t l = len(a);
+  if (i < l) return a;
+  if (i >= (1ULL << _D_LENBITS))
+    die("darr_t cannot hold more than %lu elements.", (1ULL<<_D_LENBITS));
+  _d_setlen(a,i+1);
+  size_t c = cap(a);
+  if (i < c) return a;
+  do {
+    c <<= 1;
+    _d_dblcap(a);
+  } while (i >= c);
+  a->data = _d_realloc(a->data, c * esize);
+  return a;
+}
+
+/* hash tables: use the same darr_t container.  However needs new
+   initializer because the entries need to be emptied, which will
+   confuse people.  Access is through the get function which returns a
+   pointer to the entry with the matching key.  If matching key is not
+   found one is created and initialized with _einit(key) if
+   insert=true, or NULL is returned if insert=false. */
+
+#define D_HASH(_pre, _etype, _ktype, _kmatch, _khash, _keyof, _einit, _isnull, _mknull) \
+  									\
+  static inline void _pre##clear(darr_t h) {				\
+    _etype *data = h->data;						\
+    for (size_t i = cap(h); i-- != 0; _mknull(data[i]));		\
+    _d_setlen(h,0);							\
+  }									\
+									\
+  static inline darr_t _pre##new(size_t n) {				\
+    darr_t h = darr_new(n, sizeof(_etype));				\
+    _pre##clear(h);							\
+    return h;								\
+  }									\
+									\
+  static inline void _pre##free(darr_t h) {				\
+    darr_free(h);							\
+  }									\
+									\
+  static inline size_t _d_##_pre##idx(darr_t h, _ktype k) {		\
+    size_t idx, step;							\
+    size_t mask = cap(h) - 1;						\
+    _etype *data = (_etype*) h->data;					\
+    for (idx = (_khash(k) & mask), step = 0;				\
+	 (!_isnull(data[idx]) &&					\
+	  !_kmatch(k, _keyof(data[idx])));				\
+	 step++, idx = ((idx+step) & mask));				\
+    return idx;								\
+  }									\
+  									\
+  static inline _etype *_pre##get(darr_t h, _ktype k, bool insert) {	\
+    size_t c = cap(h);							\
+    size_t l = len(h);							\
+    assert(l < c);							\
+    size_t idx = _d_##_pre##idx(h, k);					\
+    _etype *data = (_etype*) h->data;					\
+    if (!_isnull(data[idx])) return &(data[idx]);			\
+    if (!insert) return NULL;						\
+    									\
+    if (l >= (c >> 1) + (c >> 2) + (c >> 3)) {				\
+      _etype *xdata = data;						\
+      _d_dblcap(h);							\
+      size_t c2 = cap(h);						\
+      data = h->data = _d_malloc(c2 * sizeof(_etype));			\
+      for (size_t i = 0; i < c2; _mknull(data[i++]));			\
+      for (size_t j = 0; j < c; j++) {					\
+	if (_isnull(xdata[j])) continue;				\
+	size_t i = _d_##_pre##idx(h, _keyof(xdata[j]));			\
+	data[i] = xdata[j];						\
+      }									\
+      _d_free(xdata);							\
+      idx = _d_##_pre##idx(h, k);					\
+    }									\
+									\
+    data[idx] = _einit(k);						\
+    _d_inclen(h);							\
+    return &(data[idx]);						\
+  }
+
+// Define some common hash types
+#define d_strmatch(a,b) (!strcmp((a),(b)))
+#define d_eqmatch(a,b) ((a)==(b))
+#define d_keyof(a) ((a).key)
+#define d_keyisnull(a) ((a).key==NULL)
+#define d_keymknull(a) ((a).key=NULL)
+#define d_isnull(a) ((a)==NULL)
+#define d_mknull(a) ((a)=NULL)
+#define d_iszero(a) ((a)==0)
+#define d_mkzero(a) ((a)=0)
+#define d_ident(a) (a)
+extern size_t fnv1a(const char *k);
+
+#define D_STRHASH(h, etype, einit) \
+  D_HASH(h, etype, str_t, d_strmatch, fnv1a, d_keyof, einit, d_keyisnull, d_keymknull)
+
+#define D_STRSET(h) \
+  D_HASH(h, str_t, str_t, d_strmatch, fnv1a, d_ident, dstrdup, d_isnull, d_mknull)
+
+#define forhash(eptr_t, e, h, isnull) \
+  for (size_t _I_ = cap(h), _i_ = 0; _i_ < _I_; _i_++) \
+    for (eptr_t e = &(((eptr_t)((h)->data))[_i_]); ((e != NULL) && (!isnull(*e))); e = NULL)
+
+
+/* symbol table: symbols are represented with uint32_t > 0.  str2sym
+   returns 0 for strings not found if create=false.  sym2str returns
+   NULL if sym is 0 or out of range. */
+
+typedef uint32_t sym_t;
+extern sym_t str2sym(const str_t str, bool create);
+extern str_t sym2str(sym_t sym);
+extern void symtable_free();
+
+/* TODO:
+   double hash?
+   heap with linear heapify
+ */
+
+#endif  // #ifndef __DLIB_H__
+
