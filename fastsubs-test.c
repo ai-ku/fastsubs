8ebadb74381a976946e8803e0d512c60376943f8
Deniz Yuret <denizyuret@gmail.com>
2014-01-09 20:39:53 +0200

    Fixed a few incompatibilities with new code

--- fastsubs-test.c
+++ fastsubs-test.c
@@ -2,21 +2,21 @@
 #include "fastsubs.h"
 #include "heap.h"
 #include "dlib.h"
+#define SMAX 1000
 
 int main(int argc, char **argv) {
-  char buf[1024];
-  Token s[1025];
+  Token s[1+SMAX];
   msg("Loading model file %s", argv[1]);
   LM lm = lm_init(argv[1]);
-  Hpair *subs = dalloc(lm->nvocab * sizeof(Hpair));
-  msg("ngram order = %d\n==> Enter sentence:\n", lm->order);
-  while(fgets(buf, 1024, stdin)) {
-    int n = sentence_from_string(s, buf, 1024, NULL);
+  Hpair *subs = dalloc(lm_nvocab(lm) * sizeof(Hpair));
+  msg("ngram order = %d\n==> Enter sentence:\n", lm_order(lm));
+  forline (buf, NULL) {
+    int n = sentence_from_string(s, buf, SMAX, NULL);
     for (int i = 2; i <= n; i++) {
-      int nsubs = fastsubs(subs, s, i, lm, 0, 100);
-      printf("%s:\n", token_to_string(s[i]));
+      int nsubs = fastsubs(subs, s, i, lm, 1, 10);
+      printf("%s:\n", sym2str(s[i]));
       for (int j = 0; j < nsubs; j++) {
-	printf("%s\t%.4f\n", token_to_string(subs[j].token), subs[j].logp);
+	printf("%s\t%.4f\n", sym2str(subs[j].token), subs[j].logp);
       }
     }
     msg("==> Enter sentence:\n");
